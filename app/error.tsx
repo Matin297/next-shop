b16696ac9053e129a03ecff552cac749bd07185b
Matin Mir <asmamir297@gmail.com>
2024-07-28 14:25:53 +0330

    add styling to the global error page handler

--- app/error.tsx
+++ app/error.tsx
@@ -2,9 +2,11 @@
 
 export default function Error({ error }: { error: Error }) {
   return (
-    <>
-      <h2>Something went wrong!</h2>
+    <div className="space-y-4 rounded-md border border-error p-4">
+      <h2 className="text-lg font-semibold text-error">
+        Something went wrong!
+      </h2>
       <p>{error.message}</p>
-    </>
+    </div>
   );
 }
