ddd724d41ae1aafefc26aa4a3d162f2a46013814
Matin Mir <asmamir297@gmail.com>
2024-07-28 14:25:36 +0330

    add a custom not found page handler

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <p className="space-x-1 rounded-md bg-neutral-content p-4">
+      <span className="font-semibold">404</span>
+      <span>|</span>
+      <span>The requested page does not exist!</span>
+    </p>
+  );
+}
