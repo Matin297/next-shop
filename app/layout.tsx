1158a382334426cdb35fbd249e5500e16bb04e8a
Matin Mir <asmamir297@gmail.com>
2024-08-02 17:45:11 +0330

    implement a basic footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className="mx-auto max-w-6xl p-4">{children}</main>
+        <main className="mx-auto mb-4 min-h-screen max-w-6xl p-4">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-base-300 p-5 text-center">
+      FOOTER. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
+      deleniti nemo fugit ullam.
+    </footer>
+  );
+}
