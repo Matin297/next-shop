d84efd79a7fda217313ce73913d9c18c0b3c9451
Matin Mir <asmamir297@gmail.com>
2024-08-02 14:52:44 +0330

    implement increment cart item quantity server action

---
+++ actions/cart.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import db from "@/prisma/client";
+import { revalidatePath } from "next/cache";
+import { getCart, createCart } from "@/lib/data";
+
+export async function incrementQuantity(productId: string) {
+  try {
+    const cart = (await getCart()) ?? (await createCart());
+
+    const cartItem = cart.items.find((item) => item.productId === productId);
+
+    if (cartItem) {
+      await db.cartItem.update({
+        where: {
+          id: cartItem.id,
+        },
+        data: {
+          quantity: { increment: 1 },
+        },
+      });
+    } else {
+      await db.cartItem.create({
+        data: {
+          productId,
+          cartId: cart.id,
+        },
+      });
+    }
+
+    revalidatePath(`/product/${productId}`);
+
+    return {
+      message: "",
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    }
+
+    return {
+      message: "Failed to increment product quantity!",
+    };
+  }
+}
