4e9d779c9382cf651eb8104e181f4ed3f23281fb
Matin Mir <asmamir297@gmail.com>
2024-08-02 23:40:55 +0330

    home: render search and filter products based on query

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { Suspense } from "react";
+import Search from "@/components/home/search";
 import ProductList from "@/components/home/product-list";
 
-export default async function Home() {
+interface HomePageProps {
+  searchParams: {
+    q?: string;
+  };
+}
+
+export default async function Home(props: HomePageProps) {
+  const query = props.searchParams.q;
+
   return (
-    <>
-      <h2 className="mb-4 text-xl font-semibold">Products</h2>
+    <section className="space-y-4">
+      <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+        <h2 className="text-xl font-semibold">Products</h2>
+        <Search />
+      </div>
       <Suspense fallback={<p>loading...</p>}>
-        <ProductList />
+        <ProductList query={query} />
       </Suspense>
-    </>
+    </section>
   );
 }
--- components/home/product-list.tsx
+++ components/home/product-list.tsx
@@ -1,8 +1,16 @@
 import { getProducts } from "@/lib/data";
 import ProductCard from "@/components/home/product-card";
 
-export default async function ProductList() {
-  const products = await getProducts();
+interface ProductListProps {
+  query?: string;
+}
+
+export default async function ProductList({ query }: ProductListProps) {
+  const products = await getProducts({ query });
+
+  if (products.length === 0) {
+    return <p className="text-center">No result...</p>;
+  }
 
   return (
     <ul className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
