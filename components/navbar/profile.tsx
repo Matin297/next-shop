9b908b7e461ee21771b0a005bcceb838c7c658d9
Matin Mir <asmamir297@gmail.com>
2024-08-10 19:18:49 +0330

    handle profile sign in/out functionalities server side

--- components/navbar/profile.tsx
+++ components/navbar/profile.tsx
@@ -1,15 +1,13 @@
-"use client";
-
+import { auth } from "@/auth";
 import Image from "next/image";
-import { useSession } from "next-auth/react";
 import { signInAction, signOutAction } from "@/actions/auth";
 import FormSubmitButton from "@/components/form-submit-button";
 import ProfilePicturePlaceholder from "@/assets/profile-pic-placeholder.png";
 
-export default function Profile() {
-  const { status, data } = useSession();
+export default async function Profile() {
+  const session = await auth();
 
-  if (status === "unauthenticated")
+  if (!session || !session.user?.id) {
     return (
       <form action={signInAction}>
         <FormSubmitButton className="btn-outline btn-sm">
@@ -17,36 +15,35 @@ export default function Profile() {
         </FormSubmitButton>
       </form>
     );
-
-  if (status === "authenticated" && data.user) {
-    return (
-      <>
-        <form action={signOutAction}>
-          <FormSubmitButton className="btn-outline btn-sm w-full">
-            SignOut
-          </FormSubmitButton>
-        </form>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button">
-            <Image
-              width={35}
-              height={35}
-              className="rounded-full"
-              alt={data.user.name || "Unknown"}
-              src={data.user.image || ProfilePicturePlaceholder}
-            />
-          </div>
-          <div
-            tabIndex={0}
-            className="menu dropdown-content z-[1] gap-4 rounded-box bg-base-100 p-4 shadow"
-          >
-            <div>Signed in as:</div>
-            <div>{data.user?.email}</div>
-          </div>
-        </div>
-      </>
-    );
   }
 
-  return null;
+  const { user } = session;
+
+  return (
+    <>
+      <form action={signOutAction}>
+        <FormSubmitButton className="btn-outline btn-sm w-full">
+          SignOut
+        </FormSubmitButton>
+      </form>
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button">
+          <Image
+            width={35}
+            height={35}
+            className="rounded-full"
+            alt={user.name || "Unknown"}
+            src={user.image || ProfilePicturePlaceholder}
+          />
+        </div>
+        <div
+          tabIndex={0}
+          className="menu dropdown-content z-[1] gap-4 rounded-box bg-base-100 p-4 shadow"
+        >
+          <div>Signed in as:</div>
+          <div>{user?.email}</div>
+        </div>
+      </div>
+    </>
+  );
 }
